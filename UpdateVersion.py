8ee0cdbeca0bb73930b1b608ff40ac15cdeec161
david <david.kaftan@marquetteenergyanalytics.com>
2021-02-01 10:46:50 -0600

    fix style from paul's review

--- UpdateVersion.py
+++ UpdateVersion.py
@@ -1,5 +1,6 @@
 with open("VERSION", mode="r+") as f:
-  version = f.read().split(".")
-  version[-1] = str(int(version[-1]) + 1)
-  f.seek(0)
-  f.write(".".join(version))
+    version = f.read().split(".")
+    version[-1] = str(int(version[-1]) + 1)
+    f.seek(0)
+    f.write(".".join(version))
+    
